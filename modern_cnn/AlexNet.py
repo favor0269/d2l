e70a4bf1fc868ba0cbb12c889e7dfa1732890e65
favor0269 <3185537268@sjtu.edu.cn>
2026-01-05 22:13:58 +0800

    feat: AlexNet epoch=10 test_acc=0.92+

---
+++ modern_cnn/AlexNet.py
@@ -0,0 +1,246 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+from torch.utils import data
+
+import torchvision
+from torchvision import transforms
+
+import matplotlib.pyplot as plt
+
+import time
+
+
+class Timer:
+
+    def __init__(self, name=None):
+        self.name = name
+        self.start_time = None
+        self.end_time = None
+        self.elapsed = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end_time = time.time()
+        self.elapsed = self.end_time - self.start_time
+        if self.name:
+            print(f"[{self.name}] elapsed time: {self.elapsed:.2f} seconds")
+        else:
+            print(f"Elapsed time: {self.elapsed:.2f} seconds")
+
+
+def init_net(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+
+
+def create_net():
+
+    net = nn.Sequential(
+        # 这里使用一个11*11的更大窗口来捕捉对象。
+        # 同时，步幅为4，以减少输出的高度和宽度。
+        # 另外，输出通道的数目远大于LeNet
+        nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
+        nn.Conv2d(96, 256, kernel_size=5, padding=2),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        # 使用三个连续的卷积层和较小的卷积窗口。
+        # 除了最后的卷积层，输出通道的数量进一步增加。
+        # 在前两个卷积层之后，汇聚层不用于减少输入的高度和宽度
+        nn.Conv2d(256, 384, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(384, 384, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(384, 256, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2),
+        nn.Flatten(),
+        # 这里，全连接层的输出数量是LeNet中的好几倍。使用dropout层来减轻过拟合
+        nn.Linear(6400, 4096),
+        nn.ReLU(),
+        nn.Dropout(p=0.5),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(p=0.5),
+        # 最后是输出层。由于这里使用Fashion-MNIST，所以用类别数为10，而非论文中的1000
+        nn.Linear(4096, 10),
+    )
+    net.apply(init_net)
+    return net
+
+
+def show_shape():
+    X = torch.rand(size=(3, 1, 28, 28), dtype=torch.float32)
+    net = create_net()
+    for i, layer in enumerate(net):
+        X = layer(X)
+        print(f"{i}: {layer.__class__.__name__}\toutput shape: {X.shape}")
+
+
+def train_epoch(net, updater, loss, train_iter, device):
+    total_loss = 0.0
+    total_samples = 0
+    total_correct = 0
+
+    for X, y in train_iter:
+        X, y = X.to(device), y.to(device)
+
+        o_hat = net(X)
+        mean_loss = loss(o_hat, y)
+        updater.zero_grad()
+        mean_loss.backward()
+        updater.step()
+
+        # calculate data
+        total_samples += y.size(0)
+        total_loss += y.size(0) * mean_loss.item()
+        total_correct += (o_hat.argmax(dim=1) == y).sum().item()
+
+    return total_loss / total_samples, total_correct / total_samples
+
+
+def evaluate_by_test(net, loss, test_iter, device):
+    with torch.no_grad():
+        total_loss = 0.0
+        total_correct = 0
+        total_samples = 0
+        for X, y in test_iter:
+            X, y = X.to(device), y.to(device)
+
+            o_hat = net(X)
+            l = loss(o_hat, y)
+
+            # calculate data
+            total_loss += l.item() * y.size(0)
+            total_correct += (o_hat.argmax(dim=1) == y).sum().item()
+            total_samples += y.size(0)
+
+        return total_loss / total_samples, total_correct / total_samples
+
+
+def train_whole(epochs, net, updater, loss, train_iter, test_iter, device):
+    train_losses, train_accs = [], []
+    test_losses, test_accs = [], []
+
+    for i in range(epochs):
+        train_loss, train_accuracy = train_epoch(net, updater, loss, train_iter, device)
+        test_loss, test_accuracy = evaluate_by_test(net, loss, test_iter, device)
+
+        # record data
+        train_losses.append(train_loss)
+        train_accs.append(train_accuracy)
+        test_losses.append(test_loss)
+        test_accs.append(test_accuracy)
+
+        print(f"epoch {i+1}")
+        print(f"train_loss: {train_loss:.4f}, train_accuracy: {train_accuracy:.4f}")
+        print(f"test_loss: {test_loss:.4f}, test_accuracy: {test_accuracy:.4f}")
+
+    return train_losses, train_accs, test_losses, test_accs
+
+
+# define constant
+FIGSIZE = (8, 5)
+
+LOSS_COLOR_TRAIN = "tab:red"
+LOSS_COLOR_TEST = "tab:orange"
+ACC_COLOR_TRAIN = "tab:blue"
+ACC_COLOR_TEST = "tab:cyan"
+
+MARKER_LOSS = "o"
+MARKER_ACC = "x"
+
+TITLE = "Training and Testing Loss & Accuracy"
+X_LABEL = "Epoch"
+LOSS_Y_LABEL = "Loss"
+ACC_Y_LABEL = "Accuracy"
+LEGEND_LOC = "center right"
+
+
+def plot_training_curves_single_figure(
+    train_losses, train_accs, test_losses, test_accs
+):
+    epochs = range(1, len(train_losses) + 1)
+
+    _, ax1 = plt.subplots(figsize=FIGSIZE)
+
+    # left y axis：Loss
+    ax1.set_xlabel(X_LABEL)
+    ax1.set_ylabel(LOSS_Y_LABEL, color=LOSS_COLOR_TRAIN)
+    ax1.plot(
+        epochs,
+        train_losses,
+        label="Train Loss",
+        color=LOSS_COLOR_TRAIN,
+        marker=MARKER_LOSS,
+    )
+    ax1.plot(
+        epochs,
+        test_losses,
+        label="Test Loss",
+        color=LOSS_COLOR_TEST,
+        marker=MARKER_LOSS,
+    )
+    ax1.tick_params(axis="y", labelcolor=LOSS_COLOR_TRAIN)
+    ax1.grid(True)
+
+    # right y axis：Accuracy
+    # share x axis
+    ax2 = ax1.twinx()
+    ax2.set_ylabel(ACC_Y_LABEL, color=ACC_COLOR_TRAIN)
+    ax2.plot(
+        epochs, train_accs, label="Train Acc", color=ACC_COLOR_TRAIN, marker=MARKER_ACC
+    )
+    ax2.plot(
+        epochs, test_accs, label="Test Acc", color=ACC_COLOR_TEST, marker=MARKER_ACC
+    )
+    ax2.tick_params(axis="y", labelcolor=ACC_COLOR_TRAIN)
+
+    # compose the legends
+    lines_1, labels_1 = ax1.get_legend_handles_labels()
+    lines_2, labels_2 = ax2.get_legend_handles_labels()
+    ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc=LEGEND_LOC)
+
+    plt.title(TITLE)
+    plt.tight_layout()
+    plt.show()
+
+
+def decide_gpu_or_cpu():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
+    return device
+
+
+def create_updater(net):
+    return torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+
+
+def create_loss():
+    return nn.CrossEntropyLoss(label_smoothing=0.1, reduction="mean")
+
+
+device = decide_gpu_or_cpu()
+batch_size = 64
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size, resize=224)
+net = create_net().to(device)
+updater = create_updater(net)
+loss = create_loss()
+epochs = 10
+
+with Timer("Training"):
+    # train and return record
+    train_losses, train_accs, test_losses, test_accs = train_whole(
+        epochs, net, updater, loss, train_iter, test_iter, device
+    )
+
+plot_training_curves_single_figure(train_losses, train_accs, test_losses, test_accs)
